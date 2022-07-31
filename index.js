227cbd37bd10c860f6a16b6517457c298929a81f
abdullatef trefi <abdullatef.trefi98@gmail.com>
2022-07-31 17:25:54 +0300

    f

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const fs = require('fs');
+const app = express()
+
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+// const _ = require('lodash');
+app.use(fileUpload({
+    createParentPath: true
+}));
+
+//add other middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
+app.get('/',((req,res) => {
+    let form = fs.readFile('index.html',{encoding:'utf-8'},(err,data)=>{
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data)
+        return res.send();
+    })
+}));
+
+app.post('/form',((req,res)=>{
+    console.log(req.files);
+    let f = req.files.file;
+    f.mv('./uploads/'+f.name);
+    res.send('yo')
+}))
+app.listen(process.env.PORT || 8000)
